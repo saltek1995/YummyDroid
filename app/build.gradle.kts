70f80859f2665a20fe1335225f5f92f9cace107a
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-07-14 15:17:21 +0300

    Prepare YummyDroid 1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 114
-        versionName = "1.1.4"
+        versionCode = 115
+        versionName = "1.1.5"
     }
 
     buildFeatures {

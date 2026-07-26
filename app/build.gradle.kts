6339d300df2ba52962eedf2ba98276f2158ca726
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-07-26 16:13:16 +0300

    Bump version to 1.2.47

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 245
-        versionName = "1.2.45"
+        versionCode = 247
+        versionName = "1.2.47"
     }
 
     buildFeatures {

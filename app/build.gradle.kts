e7a90cbbebca2cf311e53cf3d84e59d67b5b25d4
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-07-28 02:17:06 +0300

    Bump version to 1.3.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 325
-        versionName = "1.3.25"
+        versionCode = 326
+        versionName = "1.3.26"
     }
 
     buildFeatures {

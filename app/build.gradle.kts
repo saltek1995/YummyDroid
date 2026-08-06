e8ac81739dd7aea2b086c3c188068473f4f76dd2
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-08-07 01:05:57 +0300

    Bump version to 1.3.88

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 387
-        versionName = "1.3.87"
+        versionCode = 388
+        versionName = "1.3.88"
     }
 
     buildFeatures {

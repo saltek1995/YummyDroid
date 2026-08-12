7759a520ce2c2e06439bb939b28a2a182a6f1c4d
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-08-12 23:13:25 +0300

    release v1.3.106

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 405
-        versionName = "1.3.105"
+        versionCode = 406
+        versionName = "1.3.106"
     }
 
     buildFeatures {

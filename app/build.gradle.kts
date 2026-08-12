475056518d42aa2dd14dec51b0e122fa67af2c14
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-08-12 13:12:20 +0300

    release v1.3.101

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 400
-        versionName = "1.3.100"
+        versionCode = 401
+        versionName = "1.3.101"
     }
 
     buildFeatures {

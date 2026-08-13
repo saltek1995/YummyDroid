aa6f8a718b6b66be0e5bbfa62b2fb89439ecb2dd
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-08-13 22:47:18 +0300

    chore(release): 1.3.113

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 412
-        versionName = "1.3.112"
+        versionCode = 413
+        versionName = "1.3.113"
     }
 
     buildFeatures {

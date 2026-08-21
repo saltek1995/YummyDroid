1c63291b7d4c108be77492090d275b99c9740503
saltek1995 <50554356+saltek1995@users.noreply.github.com>
2026-08-21 16:01:26 +0300

    Prepare YummyDroid 1.4.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.yummydroid.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 434
-        versionName = "1.4.19"
+        versionCode = 435
+        versionName = "1.4.20"
         buildConfigField("String", "CAST_RECEIVER_APP_ID", "\"$castReceiverAppId\"")
     }
 
